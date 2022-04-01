1268e69d45d25f70574dec91f9fa26b64a5aed2d
Gilberto Moraes <moraesdev@gmail.com>
2022-04-01 12:30:07 -0300

    add: Authentication domain and data layers
    * updates and fixes setup libs
    * Update README.md
    * feat: add authentication usecase
    * chore: add faker lib
    * refactor: change HttpPostClient to receive an object
    * feat: ensure RemototeAtuthentication calls HttpPostClient with body
    * test: feat: ensure RemototeAtuthentication calls HttpPostClient with correct body 
    * feat: ensure remote authentication throws InvalidCredentialsError on 401
    * test: ensure remote authentication throws InvalidCredentialsError on 401
    * feat: ensure remote authentication throws
    * test: ensure remote authentication throws
    * refactor: add generic params to HttpPostClient
    * feat: ensure RemoteAuthentication Returns an account on succces
    * test: ensure RemoteAuthentication Returns an account on succces
    * refactor: group imports in index files

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-# ExpoBoilerCA 
+# Boiler Clean Architecture Expo
 a boiler plate to start a project with Login using.
 Expo React Native, Hooks, Recoil, TDD, Clean Architecture, SOLID, eslint airbnb, prettier
+
+> ## Principles
+
+* Single Responsibility Principle (SRP)
+* Open Closed Principle (OCP)
+* Liskov Substitution Principle (LSP)
+* Interface Segregation Principle (ISP)
+* Dependency Inversion Principle (DIP)
+* Separation of Concerns (SOC)
+* Don't Repeat Yourself (DRY)
+* You Aren't Gonna Need It (YAGNI)
+* Keep It Simple, Silly (KISS)
+* Composition Over Inheritance
+* Small Commits
+
+> ## Design Patterns
+
+* Factory
+* Adapter
+* Composite
+* Decorator
+* Dependency Injection
+* Composition Root
+* Builder
+* Proxy
+
+> ## Methodologies and Designs
+
+* TDD
+* Clean Architecture
+* DDD
+* Reactive Programming
+* Responsive Layout
+* Conventional Commits
+* GitFlow
+* Modular Design
+* Dependency Diagrams
+* Use Cases
+* Continuous Integration
+* Continuous Delivery
+* Continuous Deployment
+
+> ## Typescript Features 
+
+* Advanced POO
+* Interface
+* Type Alias
+* Namespace
+* Module
+* Utility Types
+* Path Modularization
+* Build
+* Deploy
+* Generics
+
+> ## Tests Features
+
+* Unit test
+* Integration Test
+* E2E test 
+* Test Doubles
+* Mocks
+* Stubs
+* Spies
+* Fakes
+* Dummies
---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,102 @@
+import faker from 'faker';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { HttpPostClientSpy } from '@/data/test';
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
+import { AccountModel } from '@/domain/models';
+import { mockAccount, mockAuthentication } from '@/domain/test';
+import { AuthenticationParams } from '@/domain/usecases';
+import { RemoteAuthentication } from './remote-authentication';
+
+type SutTypes = {
+    sut: RemoteAuthentication;
+    httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>;
+};
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+    return { sut, httpPostClientSpy };
+};
+
+describe('RemoteAuthentication', () => {
+    test('Should call HttpClient with correct URL', async () => {
+        const url = faker.internet.url();
+        const { sut, httpPostClientSpy } = makeSut(url);
+
+        await sut.auth(mockAuthentication());
+
+        expect(httpPostClientSpy.url).toBe(url);
+    });
+
+    test('Should call HttpClient with correct Body', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+        const authenticationParams = mockAuthentication();
+
+        await sut.auth(authenticationParams);
+
+        expect(httpPostClientSpy.body).toEqual(authenticationParams);
+    });
+
+    test('Should throw InvalidCredencialsError if HttpPostClient returns 401', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unauthorized,
+        };
+
+        const promise = sut.auth(mockAuthentication());
+
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+    });
+
+    test('Should throw UnexpectedError if HttpPostClient returns 400', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.badRequest,
+        };
+
+        const promise = sut.auth(mockAuthentication());
+
+        await expect(promise).rejects.toThrow(new UnexpectedError());
+    });
+
+    test('Should throw notFound if HttpPostClient returns 404', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.notFound,
+        };
+
+        const promise = sut.auth(mockAuthentication());
+
+        await expect(promise).rejects.toThrow(new UnexpectedError());
+    });
+
+    test('Should throw ServerError if HttpPostClient returns 500', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.serverError,
+        };
+
+        const promise = sut.auth(mockAuthentication());
+
+        await expect(promise).rejects.toThrow(new UnexpectedError());
+    });
+
+    test('Should return an account model if status code 200', async () => {
+        const { sut, httpPostClientSpy } = makeSut();
+        const httpResult = mockAccount();
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.ok,
+            body: httpResult,
+        };
+
+        const account = await sut.auth(mockAuthentication());
+
+        expect(account).toEqual(httpResult);
+    });
+});
